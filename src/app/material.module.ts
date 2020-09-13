c4b03262f389453f397e8642e2dcccdc77c7194e
Tarik EN NAKDI <t.ennakdi@gmail.com>
2020-09-13 13:43:43 +0200

    :sparkles: add searchCity component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
 import { MatToolbarModule } from '@angular/material/toolbar'
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class MaterialModule {}
